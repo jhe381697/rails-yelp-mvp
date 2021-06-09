38da8bc844c3aed32df0f97fa3234cc3105bea3f
Josephine Herlemont <herlemont.josephine@gmail.com>
2021-06-09 16:12:43 +0200

    wip

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: 'Italian restaurant', address: 'Lille', phone_number: "0320000000", category: 'italian')
+Restaurant.create(name: 'Chinese restaurant', address:'Lille', phone_number: "0320000001", category: 'chinese')
+Restaurant.create(name: 'Japanese restaurant', address:'Lille', phone_number: "0320000002", category: 'japanese')
+Restaurant.create(name: 'French restaurant', address:'Lille', phone_number: "0320000003", category: 'french')
+Restaurant.create(name: 'Belgian restaurant', address:'Lille', phone_number: "0320000004", category: 'belgian')
